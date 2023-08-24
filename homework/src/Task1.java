325e4bb7dab8affee7b82896deceb4846004a98f
aleksandr9642827 <130972192+aleksandr9642827@users.noreply.github.com>
2023-08-24 23:49:15 +0300

    godewo

---
+++ homework/src/Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int ticketPrice = 62662;
+        int mile = ticketPrice / 20;
+        System.out.print("Кол-во бонусных миль = ");
+        System.out.println(mile);
+    }
+}
